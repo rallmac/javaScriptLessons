df0475f8f7ca59d64b3d11f5cc03c5ead0e2127d
rallmac <tobijahekperikpe@gmail.com>
2025-10-26 06:19:04 +0100

    Examples and exercises with headfirst javaScript(ECMA)

---
+++ MyFirstApp.java
@@ -0,0 +1,14 @@
+public class MyFirstApp {
+
+    public static void main (String[] args) {
+        int x = 27;
+        int i;
+        while(x > 10) {
+            while (i = x) {
+                i++;
+            }
+            System.out.println(x + " Shake your Ass " + i);
+            x = x - 1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my javascript tutorial and practice files using headfirst javaScript
